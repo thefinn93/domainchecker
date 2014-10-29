6e258d7a67244bc56c0642680ebdaf9fd482ed71
Finn <thefinn93@thefinn93.com>
2014-10-29 00:54:14 -0700

    Added a module to check for how the site handles www.<domain>

---
+++ modules/www.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import logging
+
+class Module(object):
+    def __init__(self):
+        self.name = "www"
+        self.dependencies = ["requests"]
+        self.author = {
+          "name": "Finn Herzfeld",
+          "email": "finn@finn.io",
+          "url": "https://finn.io"
+        }
+
+    def check(self, domain):
+        import requests
+        from urlparse import urlparse
+
+        score = 0
+        reason = ""
+
+        nakedreq = requests.get("http://%s" % domain)
+        nakedreqURL = urlparse(nakedreq.url)
+        wwwreq = requests.get("http://www.%s" % domain)
+        wwwreqURL = urlparse(wwwreq.url)
+
+
+        if nakedreqURL.netloc == domain:
+            score = 0
+            reason = "Naked domain (%s) does not redirect to www (but www does not redirect to naked)" % domain
+            if wwwreqURL.netloc == domain:
+                score = 1
+                reason = "www.%s redirects to %s" % (domain, domain)
+        elif nakedreqURL.netloc == "www.%s" % domain:
+            score = -1
+            reason = "Naked domain redirects to www."
+
+        return {
+            "score": score,
+            "reason": reason
+        }
