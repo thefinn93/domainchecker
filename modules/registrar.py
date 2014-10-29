4bdee2301130f4af33b8cac34be9512732a42191
Finn <thefinn93@thefinn93.com>
2014-10-29 01:39:01 -0700

    got registrar module working on python3

--- modules/registrar.py
+++ modules/registrar.py
@@ -11,7 +11,7 @@ class Module(object):
         """The __init__ method only needs to define self.name as the name of the
         plugin. More requirements may come later"""
         self.name = "Registrar"
-        self.dependencies = ["pythonwhois"]
+        self.dependencies = ["whois"]
         self.author = {
           "name": "Finn Herzfeld",
           "email": "finn@finn.io",
@@ -19,18 +19,21 @@ class Module(object):
         }
 
     def check(self, domain):
-        import pythonwhois
+        import whois
         """Looks up the whois info for the domain in question and attempts to extract the registrar"""
-        query = pythonwhois.get_whois(domain)
         score = 0
         reason = "Registrar could not be determined"
-        if "registrar" in query:
-            for registrar in query['registrar']:
-                if "godaddy" in registrar.lower():
-                    score = -2
-                    reason = "Registrar, %s, is known to be evil and of low quality. Additionally, their CEO hunts elephants and they support SOPA." % registrar
+
+        try:
+            registrar = whois.query(domain).registrar
+            if "godaddy" in registrar.lower():
+                score = -2
+                reason = "Registrar, %s, is known to be evil and of low quality. Additionally, their CEO hunts elephants and they support SOPA." % registrar
             if score == 0: # registrar not listed
                 reason = "The registrar, %s, is not known to be particularly bad or good. If you feel this is an oversight on our part, feel free to file a bug or pull request" % registrar
+        except Exception as e:
+            score = 0
+            reason = e
 
 
         result = {
